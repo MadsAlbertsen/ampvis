313b1531bb8adcdf4b5d602eee28157952e58914
Mads Albertsen <MadsAlbertsen85@gmail.com>
2014-04-16 13:04:12 +0200

    Added correlation function

--- R/amp_correlation.R
+++ R/amp_correlation.R
@@ -1,24 +1,22 @@
 
-#' Generates a ggplot2 style ordinate plot of amplicon data.
+#' Generates correlation networks from amplicon data.
 #'
 #' A nice long description
 #'
 #' @usage amp_rabund(data)
 #'
 #' @param data (required) A phyloseq object including sample data.
-#' @param trans Transform the raw counts, currently only supports "none" or "sqrt" (default: "sqrt").
-#' @param ordinate.type Either PCA or NMDS (default: "PCA").
-#' @param ncomp The number of principal components to extract (default: 5)
-#' @param plot.x Variable to plot on the x-axis (default: "PC1")
-#' @param plot.y Variable to plot on the y-axis (default: "PC2")
-#' @param plot.color Color the points by a sample variable.
-#' @param plot.point.size Size of the plotted points (default: 3)
-#' @param plot.species Plot loadings as points (default: False)
-#' @param plot.nspecies Plot the n most extreme species with their genus classification (default: 0).
-#' @param envfit.factor A vector of factor variables from the sample data used for envfit to the model.
-#' @param envfit.numeric A vector of numerical variables from the sample data used for envfit to the model.
-#' @param envfit.significant The significance treshold for displaying envfit parameters (default: 0.01).
-#' @param tax.clean Add best assignment to Genus level classification if none exists.
+#' @param trans Transform the raw counts, currently only supports "none" or "sqrt" (default: "none").
+#' @param treshold.cor Absolute correlation treshold (default: 0.8).
+#' @param treshold.count Minimum count in order to use a set of observations for correlation significance test (default: 10)
+#' @param treshold.pval P-value treshold for correlations (default: 0.01).
+#' @param treshold.abundance Average abundance treshold (default: 0) 
+#' @param ignore.zero Remove observations with 0 counts from the correlation test (default: T)
+#' @param tax.clean Assign best classification to OTUs (default: T).
+#' @param label Plot taxonomic classifications (e.g. "Genus") instead of points (default: "OTU")
+#' @param scale.abundance Scale the size of nodes by abundance (default: F).
+#' @param scale.seq The number of sequences, used to calculate abundances (default: 20000)
+#' @param scale.size Scale the size of the plotted objects (default: 1)
 #' @param output Either plot or complete (default: "plot").
 #' 
 #' @return A ggplot2 object
@@ -26,17 +24,15 @@
 #' @export
 #' @import ggplot2
 #' @import plyr
+#' @import igraph
 #' @import reshape2
 #' @import phyloseq
 #' @import grid
-#' @import vegan
 #' 
 #' @author Mads Albertsen \email{MadsAlbertsen85@@gmail.com}
 
-data <- V13f
-cor.treshold <- 0.8
 
-amp_ordinate <- function(data, trans = "sqrt"){
+amp_correlation <- function(data, trans = "none", treshold.cor = 0.8, treshold.pval = 0.01, tax.clean = T, ignore.zero = T, scale.seq = 20000, treshold.abundance = 0, label = NULL, scale.abundance = F, output = "plot", treshold.count = 10, scale.size = 1){
   
   ## Load the data
   
@@ -46,11 +42,43 @@ amp_ordinate <- function(data, trans = "sqrt"){
   
   outlist <- list(abundance = abund, taxonomy = tax, sampledata = sample)
   
+  ## Clean taxonomy to give the best genus classification
+  
+  if (tax.clean == T){
+    t <- tax
+    t$Kingdom <- as.character(t$Kingdom)
+    t$Phylum <- as.character(t$Phylum)
+    t$Class <- as.character(t$Class)
+    t$Order <- as.character(t$Order)
+    t$Family <- as.character(t$Family)
+    t$Genus <- as.character(t$Genus)
+    t[is.na(t$Phylum),"Phylum"] <- "p__Unclassified"
+    a <- which( colnames(t)=="Class")
+    for (i in 1:nrow(t)){
+      if (t[i,a-1] == "p__"){t[i,a-1] <- "p__Unclassified"}
+      if (is.na(t[i,a])){t[i,a] <- t[i, a-1]}
+      if (t[i,a] == "c__"){t[i,a] <- t[i, a-1]}
+      if (is.na(t[i,a+1])){t[i,a+1] <- t[i, a]}
+      if (t[i,a+1] == "o__"){t[i,a+1] <- t[i, a]}
+      if (is.na(t[i,a+2])){t[i,a+2] <- t[i, a+1]}
+      if (t[i,a+2] == "f__"){t[i,a+2] <- t[i, a+1]}
+      if (is.na(t[i,a+3])){t[i,a+3] <- t[i, a+2]}  
+      if (t[i,a+3] == "g__"){t[i,a+3] <- t[i, a+2]}
+    }  
+    tax <-t  
+  }
+
+  tax <- cbind.data.frame("OTU" = rownames(tax), tax)
+  
   ## Transform the data
   
   abund1 <- abund
   if (trans == "sqrt"){
     abund1 <- sqrt(abund)
+    treshold.count <- sqrt(treshold.count)
+    if (treshold.abundance != 0){
+      treshold.abundance <- sqrt(treshold.abundance)
+    }
   }
   
   ## Calculate correlation matrix
@@ -60,24 +88,110 @@ amp_ordinate <- function(data, trans = "sqrt"){
   cor2 <- melt(cor)
   cor3 <- cor2[!is.na(cor2$value), ]
   cor4 <- cor3[cor3$Var1 != cor3$Var2, ]
-  cor5 <- subset(cor4, abs(value) > cor.treshold)
+  
+  ## Subset based on correlation
+  
+  cor5 <- subset(cor4, abs(value) >= treshold.cor)
+  cor6 <- cbind.data.frame(cor5, pval = rep(1, nrow(cor5)), nobs = rep(0, nrow(cor5)))  
+  
+  ## Make formal correlation test of the subset of points
+  
+  for (i in 1:nrow(cor6)){
+    x <- as.numeric(abund1[rownames(abund1)==cor6$Var1[i],])
+    y <- as.numeric(abund1[rownames(abund1)==cor6$Var2[i],])
+    c <- cbind(x,y)
+    if (ignore.zero == T){
+      c[is.na(c)] <- NA
+    }
+    c1 <- subset(c, !is.na(x) & !is.na(y))
+    c2 <- subset(c1, x+y >= treshold.count)
+    if (nrow(c2) > 4){
+      cor6$pval[i] <- cor.test(c2[,1],c2[,2])$p.value*nrow(abund)  
+    }  
+    if (!is.null(nrow(c2))) { cor6$nobs[i] <- nrow(c2)}
+  }
+  
+  cor7 <- subset(cor6, pval <= treshold.pval)
+  
+  outlist <- append(outlist, list(correlation = cor7))
+  
+  ## Format the data to graph format using igraph
+    
+  g <- graph.data.frame(cor7, directed = F)
+  
+  ## Calculate a graph   
+  
+  t <- layout.fruchterman.reingold(g)
+  
+  ## Extract the individual points in the graph and add abundance 
+    
+  gpoints <- data.frame( "OTU" = V(g)$name, "x" = t[,1], "y" = t[,2])
+  gpoints1 <-merge(gpoints, tax)
+  
+  TotalSum <- apply(abund, 1, function(x) mean(x)/scale.seq*100)
+  TotalSum1 <- data.frame("OTU" = names(TotalSum), "Abundance" = TotalSum)
+  
+  gpoints2 <- merge(TotalSum1, gpoints1)
+  
+  ## Subset based on average abundance
+  
+  gpoints3 <- subset(gpoints2, Abundance >= treshold.abundance)
+  
+  ## Extract links between scaffolds
+  
+  seq <- merge(cor7, gpoints3[,c(1,3,4)], by.x = "Var1", by.y = "OTU")
+  seg1 <- merge(seq, gpoints3[,c(1,3,4)], by.x = "Var2", by.y = "OTU")
+  colnames(seg1) <- c("OTU1","OTU2","Correlation","pval", "nobs", "x", "y", "xend", "yend")
+  
+  outlist <- append(outlist, list(nodes = gpoints, edges = seg1))
+  
+  ## Make a ggplot2 object
+  
+  p <- ggplot(data = gpoints3, aes(x = x, y = y)) +
+    geom_segment(data=seg1, aes(x=x, y=y, xend=xend, yend=yend, color = Correlation), size = 1) +
+    geom_point(size = 4*scale.size) +
+    scale_color_continuous(low="red", high = "green", limits=c(-1,1), name = "Correlation (r)")
+  
+  
+  if (!is.null(label)){
+    p <- ggplot(data = gpoints3, aes_string(x = "x", y = "y", label = label)) +
+      geom_segment(data=seg1, aes(x=x, y=y, xend=xend, yend=yend, label = NA, color = Correlation), size = 1) +
+      geom_text(size = 4*scale.size, color = "black") +
+      scale_color_continuous(low="red", high = "green", limits=c(-1,1), name = "Correlation (r)")
+  }
+  
+  if (scale.abundance == T & is.null(label)){
+    p <- ggplot(data = gpoints3, aes(x = x, y = y, size = Abundance)) +
+      scale_size_area(name = "Abundance (%)", max_size=20*scale.size, breaks = c(0.01, 0.05, 0.1, 0.5, 1)) +
+      geom_segment(data=seg1, aes(x=x, y=y, xend=xend, yend=yend, color = Correlation), size = 1) +
+      geom_point() +
+      scale_color_continuous(low="red", high = "green", limits=c(-1,1), name = "Correlation (r)")
+  }
 
+  if (scale.abundance == T & !is.null(label)){
+    p <- ggplot(data = gpoints3, aes_string(x = "x", y = "y", size = "Abundance", label = label)) +
+      scale_size_area(name = "Abundance (%)", max_size=10*scale.size, breaks = c(0.01, 0.05, 0.1, 0.5, 1)) +
+      geom_segment(data=seg1, aes(x=x, y=y, xend=xend, yend=yend, color = Correlation, label = NA), size = 1) +
+      geom_text(color = "black") +
+      scale_color_continuous(low="red", high = "green", limits=c(-1,1), name = "Correlation (r)")
+  }  
   
-  g <- graph.data.frame(cor5, directed = F)
+  ## Remove background lines
   
-  V(g)$label.cex <- 0.7
-  V(g)$label.color <- "black"
-  V(g)$label.font <- 2
-  V(g)$frame.color <- "black"
-  V(g)$color <- "white"
-  V(g)$size <- 10
-  E(g)$width <- 2
-  E(g)$color <- (E(g)$value + 1) * 100
+  p <- p + 
+    theme_bw() + 
+    theme(panel.grid.major=element_blank(), 
+          panel.grid.minor=element_blank(), 
+          axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          axis.ticks=element_blank(),
+          panel.border=element_blank())
   
-  rwg <- colorRampPalette(c("red", "white", "green"))
-  palette(rwg(100))
+  outlist <- append(outlist, list(plot = p))
   
-  plot(g)
-  
-  
+  if (output == "complete"){ return(outlist) }
+  if (output == "plot"){ return(p) }
 }
+
