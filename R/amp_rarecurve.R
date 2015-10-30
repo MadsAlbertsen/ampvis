3bf246ba4d99ce64d7472b25b8a662e81263bbd1
Rasmus Kirkegaard <rasmus.kirkegaard@hotmail.com>
2015-10-30 15:03:48 +1000

    Update amp_rarecurve.R
    Suggestion to add colours to the lines of amp_rarecurve based on a grouping variable

--- R/amp_rarecurve.R
+++ R/amp_rarecurve.R
@@ -16,20 +16,45 @@
 #' 
 #' @author Mads Albertsen \email{MadsAlbertsen85@@gmail.com}
 
-amp_rarecurve <- function(data, step = 100, ylim = NULL, xlim = NULL, label = F){
+amp_rarecurve <- function(data, step = 100, ylim = NULL, xlim = NULL, label = F,color_group=NULL){
   
   abund = otu_table(data)@.Data %>% as.data.frame()
   
+  if (!is.null(color_group)) {
+    gg_color_hue <- function(n) {
+      hues = seq(15, 375, length=n+1)
+      hcl(h=hues, l=65, c=100)[1:n]
+    }
+
+    group_vector<-sample_data(data)[,color_group]@.Data %>% as.data.frame()
+    names(group_vector)<-"color_variable"
+    group_vector<-as.character(group_vector$color_variable)
+    groups<-unique(group_vector)
+    n = length(groups)
+    cols = gg_color_hue(n)
+
+    col_vector<-rep("black",length(group_vector))
+    for (i in 1:length(group_vector)){
+    col_vector[i]<-cols[match(group_vector[i],groups)]
+    }
+  } else {
+  cols="black"
+  }
+  
+  
   if (is.null(ylim) & is.null(xlim)){
-    rarecurve(t(abund), step = step, label = label)
+    rarecurve(t(abund), step = step, label = label, col = cols)
   }
   if (!is.null(ylim) & !is.null(xlim)){
-    rarecurve(t(abund), step = step, ylim = ylim, xlim = xlim, label = label)
+    rarecurve(t(abund), step = step, ylim = ylim, xlim = xlim, label = label, col = cols)
   }
   if (!is.null(ylim) & is.null(xlim)){
-    rarecurve(t(abund), step = step, ylim = ylim, label = label)
+    rarecurve(t(abund), step = step, ylim = ylim, label = label, col = cols)
   }
   if (is.null(ylim) & !is.null(xlim)){
-    rarecurve(t(abund), step = step, xlim = xlim, label = label)
+    rarecurve(t(abund), step = step, xlim = xlim, label = label, col = cols)
+  }
+  if (!is.null(group_color)) {
+    legend("topleft",legend = groups,fill = cols)
   }
 }
